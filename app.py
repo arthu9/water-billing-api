85e026b95873144af4dfb50a11e31b451f0117ef
Joren Mundane <jorenpacaldo@yahoo.com>
2018-10-04 02:01:05 +0800

    go earl!

--- app.py
+++ app.py
@@ -63,4 +63,6 @@ def add_cors(resp):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.debug=True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
